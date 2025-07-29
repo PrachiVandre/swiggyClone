1ad169d9ff6afa837670ac0d13db84ab93a0836a
Prachi Vandre <prachivandre@gmail.com>
2025-07-29 12:01:42 +0530

    Updated Constant.jsx

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,14 +1,20 @@
+// src/utils/constant.jsx
+
 const proxy = "https://thingproxy.freeboard.io/fetch/";
 
-const RESTAURANTS_ENDPOINT = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-const MENU_ENDPOINT_BASE = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.52110&lng=73.85020&restaurantId=";
+// Swiggy endpoints
+const RESTAURANTS_ENDPOINT =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const API_URL = proxy + encodeURIComponent(RESTAURANTS_ENDPOINT);
-export const SINGLE_PAGE = (restaurantId) => proxy + encodeURIComponent(MENU_ENDPOINT_BASE + restaurantId);
+const MENU_ENDPOINT_BASE =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.52110&lng=73.85020&restaurantId=";
 
+// Do not encode the endpoint when using thingproxy
+export const API_URL = proxy + RESTAURANTS_ENDPOINT;
+export const SINGLE_PAGE = (restaurantId) =>
+  proxy + MENU_ENDPOINT_BASE + restaurantId;
 
-export const CARD_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const MENU_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"
-
-
+export const CARD_IMG =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const MENU_IMG =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
